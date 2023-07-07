c5ca0072312f1e7963cab030cb8e213d45139e57
areed <areed@DESKTOP-OJC2AO6.Aeye.local>
2023-07-06 18:25:13 -0700

    add plot_xyz

--- Data/plot_xyz.m
+++ Data/plot_xyz.m
@@ -1,6 +1,7 @@
 function [outputArg1,outputArg2] = plot_xyz(xyz, frame_num)
 figure; 
 scatter3(xyz{frame_num}(:,1),xyz{frame_num}(:,2),xyz{frame_num}(:,3),10,xyz{frame_num}(:,4),'filled'); 
-colorbar; ylim([-5 5]); zlim([-2 5]); view(-90,90); xlabel("X (m)");ylabel("Y (m)");zlabel("Z (m)"); grid minor;
+colorbar; xlim([0 80]); zlim([-8 5]); ylim([-10 5]); xlabel("X (m)");ylabel("Y (m)");zlabel("Z (m)"); grid minor;
+% ; zlim([-2 5]); view(-90,90)
 end
 
